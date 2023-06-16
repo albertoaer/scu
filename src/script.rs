545fa4d00b8e48422b9fa69e0094b6fe1362edf8
Alberto Elorza <albertoelorza2002@gmail.com>
2023-06-16 14:34:36 +0200

    updated separators

--- src/script.rs
+++ src/script.rs
@@ -30,7 +30,7 @@ macro_rules! script_display {
             $(Interpreter::$variant)|* => write!(
               f,
               $template,
-              self.binary,
+              script_options_display!([&self.binary], $($options)*),
               script_options_display!(self.args, $($options)*)
             ),
           )*
@@ -42,28 +42,33 @@ macro_rules! script_display {
 }
 
 macro_rules! script_options_display {
-  ($source:expr, sep $separator:literal wrap $wrapper:literal) => {
-    $source.iter().map(|x| format!($wrapper, x)).collect::<Vec<String>>().join($separator)
-  };
+  ($source:expr, sep $separator:literal wrap-spaces $wrapper:literal) =>
+    ($source.iter().map(
+      |x| if x.contains(" ") { format!($wrapper, x) } else { x.to_string() }
+    ).collect::<Vec<String>>().join($separator));
+  ($source:expr, sep $separator:literal wrap $wrapper:literal) =>
+    ($source.iter().map(
+      |x| format!($wrapper, x)
+    ).collect::<Vec<String>>().join($separator));
 }
 
 script_display! {
   Bash => "#!/bin/sh
-\"{}\" {} \"$@\"
-exit $?" [sep " " wrap "\"{}\""]
+{} {} \"$@\"
+exit $?" [sep " " wrap-spaces "\"{}\""]
 
   Cmd => "@ECHO off
-\"{}\" {} %*
-EXIT /b %errorlevel%" [sep " " wrap "\"{}\""]
+{} {} %*
+EXIT /b %errorlevel%" [sep " " wrap-spaces "\"{}\""]
 
   Python Pythonw => "from subprocess import run
 from sys import argv
 
-program = [\"{}\", {}]
+program = [{}, {}]
 
 code = run(program + argv[1:]).returncode
 exit(code)" [sep ", " wrap "\"{}\""]
 
-  Powershell => "& \"{}\" {} $args
-exit $LASTEXITCODE" [sep " " wrap "\"{}\""]
+  Powershell => "& {} {} $args
+exit $LASTEXITCODE" [sep " " wrap-spaces "\"{}\""]
 }
