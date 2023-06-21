7367f4005cdca7105d70d80dfca85932135ec60b
Alberto Elorza <albertoelorza2002@gmail.com>
2023-06-21 02:13:24 +0200

    handled stdin reader error

--- src/reader.rs
+++ src/reader.rs
@@ -1,12 +1,13 @@
-use std::{io::{self, Read}, fs::{read}, path};
+use std::{io::{self, Read}, fs::read, path};
 
 use crate::errors::Result;
 
 pub fn from_stdin() -> Result<String> {
   let mut handle = io::stdin().lock();
   let mut input = Vec::new();
-  handle.read_to_end(&mut input);
-  String::from_utf8(input).map_err(|err| err.into())
+  handle.read_to_end(&mut input).map_err(|err| err.into()).and_then(
+    |_| String::from_utf8(input).map_err(|err| err.into())
+  )
 }
 
 pub fn from_file(source: impl AsRef<path::Path>) -> Result<String> {
